4145d993950a37a85289d9d6cfc2b9abeaca109b
aditditkudit <adityaadit92@gmail.com>
2019-03-03 16:09:59 +0700

    fix in res.partner and add ir.sequence

--- models/res_partner.py
+++ models/res_partner.py
@@ -12,10 +12,19 @@ class res_partner(models.Model):
     pekerjaan = fields.Selection(string='Pekerjaan', selection=[('pelajar_mhs', 'Pelajar/Mahasiswa'), ('pegawai_negri', 'Pegawai Negri'),('swasta','Swasta'),('lain','Lain-Lain')],)
     jenis_kelamin = fields.Selection(string='Jenis Kelamin', selection=[('laki', 'Laki-Laki'), ('perempuan', 'Perempuan'),])
 
-    _sql_constraint = [
+    _sql_constraints = [
         ('unique_kode', 'unique(kode,name)', 'Kombinasi Kode dan nama sudah ada, mohon di cek kembali'),
     ]
 
+    @api.model
+    def create(self, values):
+        # Add code here
+        if values.get('customer', False):
+            values['kode'] = self.env['ir.sequence'].get_sequence('Kode Customer','res.partner','CSTR')
+        elif values.get('montir', False) and ('kode' not in values or values.get('kode', False) == '/'):
+            values['kode'] = self.env['ir.sequence'].get_sequence('Kode Montir','res.partner', 'MNTR')
+        return super(res_partner, self).create(values)
+    
     
     
     
